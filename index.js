f89b390548e1335332758074530efc4fdef7d0c2
Lorenzo Fernandez <lorenzo.kniss@gmail.com>
2019-10-06 17:49:45 -0300

    Created a folder to separate the source files

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("App is running.");
+import BinarySearchTree from "./binary-search-tree";
+
+let tree = new BinarySearchTree();
+     
+tree.insert(null, 12);
+tree.insert(tree.root, 4);
+tree.insert(tree.root, 2);
+tree.insert(tree.root, 8);
+tree.insert(tree.root, 6);
+tree.insert(tree.root, 16);
+
+console.log("\nIn Order:");
+tree.inOrder(tree.root);
+
+console.log("\nPre Order:");
+tree.preOrder(tree.root);
+
+console.log("\nPost Order:");
+tree.postOrder(tree.root);
+
+
+let test = tree.search(tree.root, 3);
+console.log(test);
